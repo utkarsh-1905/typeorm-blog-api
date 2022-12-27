56cb3a3a60c6af8f56fa7a7a312d61b874320c2e
Utkarsh <utripathi2002@gmail.com>
2022-12-27 20:08:46 +0530

    env added to data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,13 +5,17 @@ import { Comments } from "./entities/Comments";
 import { Profile } from "./entities/Profile";
 import { Auth } from "./entities/Auth";
 
+if (process.env.NODE_ENV === "production") {
+  require("dotenv").config();
+}
+
 export const dataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "admin",
-  database: "postgres",
+  host: process.env.HOST,
+  port: parseInt(process.env.PORT!),
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   entities: [Users, Blogs, Comments, Profile, Auth],
   synchronize: true,
   logging: ["error", "migration"],
